b0d120c57df9a0c39ec0312b3436247bc8ce05f6
Mauricio Velazquez <mauri_i9070@hotmail.com>
2026-08-06 20:34:36 -0300

    backend-java

---
+++ backend-benteveo/src/main/java/ar/com/benteveo/backend/BackendBenteveoApplication.java
@@ -0,0 +1,13 @@
+package ar.com.benteveo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBenteveoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBenteveoApplication.class, args);
+    }
+
+}
