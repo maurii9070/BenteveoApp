4de9ae5c86c622aa9de59557478074c72db32b66
RocioAV <rocio.velazquez.0212@gmail.com>
2026-08-07 23:38:18 -0300

    Configure application properties for PostgreSQL datasource and update Product entity by removing reviews relationship

--- backend-benteveo/src/main/java/ar/com/benteveo/backend/entities/Product.java
+++ backend-benteveo/src/main/java/ar/com/benteveo/backend/entities/Product.java
@@ -92,6 +92,5 @@ public class Product {
     @OneToMany(mappedBy = "product")
     private List<Reservation> reservations;
 
-    @OneToMany(mappedBy = "product")
-    private List<Review> reviews;
+
 }
