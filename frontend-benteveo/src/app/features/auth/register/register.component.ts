7bc076f10a56fc879364bbebedd107ec5636dfa7
RocioAV <rocio.velazquez.0212@gmail.com>
2026-08-11 00:53:55 -0300

    feat: enhance registration form with validation and responsive design

--- frontend-benteveo/src/app/features/auth/register/register.component.ts
+++ frontend-benteveo/src/app/features/auth/register/register.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  private readonly fb = inject(FormBuilder);
+
+  readonly registerForm = this.fb.nonNullable.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    dni: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.getRawValue());
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+}
